9686e9b65524f63c5ae46d7f3a695bb020b65c08
HarjotSahota <93726984+HarjotSahota@users.noreply.github.com>
2025-02-16 20:15:59 -0800

    Launch Indicator

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -5,6 +5,9 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform ballAnchor;
+    [SerializeField] private Transform launchIndicator;
+
 
     private Rigidbody ballRB;
     private bool isBallLaunched;
@@ -18,12 +21,24 @@ public class BallController : MonoBehaviour
         // When the space key is pressed the
         // LaunchBall method will be called.
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+
+        ballRB.isKinematic = true;
+
     }
 
     private void LaunchBall()
     {
         if (isBallLaunched) return;
         isBallLaunched = true;
+        transform.parent = null;
+        ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
+        launchIndicator.gameObject.SetActive(false);
+
+        ballRB.isKinematic = false;
+        // this sets the object to the outermost layer of the hierarchy
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
 
     }
